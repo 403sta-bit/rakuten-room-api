4cd6dd4d610de556f42f3cf6281f44b7e4d4195e
403sta-bit <402sta@gmail.com>
2026-08-07 20:18:27 +0900

    Fix headers syntax

--- api/search.js
+++ api/search.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
     const response = await fetch(url, {
       method: "GET",
       headers: {
-        accessKey: accessKey
+        accessKey: accessKey,
         "Referer": "https://rakuten-room-api.vercel.app/"
       }
     });
